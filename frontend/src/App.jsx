569bd501c53c3a2f722782e71e6fc31125df5cde
Gregory Baumes <baumesg@sunypoly.edu>
2023-11-18 13:18:36 -0500

    Added about route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 // components
 import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
+import About from "./pages/About";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <div className="pages">
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
           </Routes>
         </div>
       </BrowserRouter>
