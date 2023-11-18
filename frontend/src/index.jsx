cdd7052ddac8b734f5bb900c26e7fd919dad03ff
Gregory Baumes <baumesg@sunypoly.edu>
2023-11-17 20:14:21 -0500

    added index.css

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -4,6 +4,7 @@ import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap";
 import { WeatherProvider } from "./contexts/WeatherContext";
+import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
