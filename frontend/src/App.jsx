b1b9e49f8315539ea1d6cb23c26da65612e0f125
Gregory Baumes <baumesg@sunypoly.edu>
2023-11-18 13:35:14 -0500

    Added radar routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
 import About from "./pages/About";
+import Radar from "./pages/Radar";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
+            <Route path="/radar" element={<Radar />} />
           </Routes>
         </div>
       </BrowserRouter>
