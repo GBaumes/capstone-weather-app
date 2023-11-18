2a1658e1f2635efe44af7bb207d959db017053cd
Gregory Baumes <baumesg@sunypoly.edu>
2023-11-18 14:47:43 -0500

    Created Footer.jsx

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="container">
+      <footer className="text-center">
+        <a
+          href="https://icons8.com"
+          className="text-decoration-none"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          All icons by Icons8
+        </a>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
