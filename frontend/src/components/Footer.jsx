d31ed2304bf5776108fbc8e9287db43f3a404df1
Gregory Baumes <baumesg@sunypoly.edu>
2023-11-18 17:16:54 -0500

    Removed container

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -2,18 +2,16 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="container">
-      <footer className="text-center">
-        <a
-          href="https://icons8.com"
-          className="text-decoration-none"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          All icons by Icons8
-        </a>
-      </footer>
-    </div>
+    <footer className="text-center">
+      <a
+        href="https://icons8.com"
+        className="text-decoration-none"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        All icons by Icons8
+      </a>
+    </footer>
   );
 };
 
