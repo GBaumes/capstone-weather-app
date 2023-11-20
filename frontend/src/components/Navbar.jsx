6264bfe2726d80ce2b2f7412fa83048c6f4aa3e5
GBaumes <baumesg@sunypoly.edu>
2023-11-20 15:54:25 -0500

    removed onclickevent from home button

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -35,7 +35,7 @@ const Navbar = () => {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <Link className="nav-link" to="/" onClick={handleIconClick}>
+              <Link className="nav-link" to="/">
                 Home
               </Link>
             </li>
