39e830ea60679a5f39b6ccff4fbe81ab89453205
GBaumes <baumesg@sunypoly.edu>
2023-11-19 23:12:56 -0500

    Added event handler to home button

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -36,7 +36,7 @@ const Navbar = () => {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <Link className="nav-link" to="/">
+                <Link className="nav-link" to="/" onClick={handleIconClick}>
                   Home
                 </Link>
               </li>
